be811fdc3dece9b591b8acadd84d8bb388664e19
Daniel Fernando Guimarães Rodrigues <danifgr@gmail.com>
2019-10-15 14:26:08 -0400

    Separating Containers

---
+++ src/components/containers/PokemonSelector.js
@@ -0,0 +1,12 @@
+import react from 'React'
+
+const PokemonSelector = () => {
+
+    const content = (
+            <h1>Pokémon</h1>
+    )
+
+    return content
+}
+
+export default PokemonSelector
---
+++ src/components/containers/StringCreated.js
@@ -0,0 +1,11 @@
+import React, {useContext} from 'react'
+
+const StringCreated = () => {
+    const { string, setString } = useContext(StringContext)
+
+    const content = ()
+
+    return content
+}
+
+export default StringCreated
